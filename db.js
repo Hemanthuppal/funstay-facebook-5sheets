0e232052cb055d15fda3e5d9c8ad8369339cadea
Hemanthuppal <131530809+Hemanthuppal@users.noreply.github.com>
2025-06-14 11:16:00 +0530

    db

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 // db.js
 const mysql = require('mysql2');
 
-// const dbConfig = {
-//   host: 'localhost',
-//   user: 'nodeuser',
-//   password: 'Root@1234',
-//   database: 'funstay_db',
-// };
-
-
 const dbConfig = {
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'funstaydb',
+  user: 'nodeuser',
+  password: 'Root@1234',
+  database: 'funstay_db',
 };
+
+
+// const dbConfig = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'funstaydb',
+// };
 
 function createConnection() {
   return mysql.createConnection(dbConfig);
